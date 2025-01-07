8d989066dee9a390be02993cc4c50918f0f34b1d
dappdev20 <olehden20@gmail.com>
2025-01-07 01:33:22 +0100

    refine code

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { BiSolidDownArrow } from "react-icons/bi";
 import Logo from "../public/Logo.svg";
 import Swap from "./assets/Swap";
